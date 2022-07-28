33974868c87ec8c9742bed53bd35271071da859e
wanjala stephen <stephenwanjala145@gmail.com>
2022-07-28 23:18:22 +0300

    top level variables

--- topLevelVariable.kt
+++ topLevelVariable.kt
@@ -1,11 +1,15 @@
 /*You can declare variables at the top level. */
 val PI = 3.14
 var x = 0
+var name="stephen"
+val school:String="MySchool"  //string
+val isMale=true //bolean
+
 
 fun addOneToX(){
     x+=1
 }
-/*not famillia with functions? checkout to the `functions`  branch*/
+/*not familliar with functions? checkout to the `functions`  branch*/
 fun main(args: Array<String>) {
     println("x = $x; PI = $PI")
 addOneToX()
