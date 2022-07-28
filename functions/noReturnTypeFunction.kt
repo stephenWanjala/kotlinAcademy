80f802b83167b3909547d70aedcec1445de2adb9
wanjala stephen <stephenwanjala145@gmail.com>
2022-07-29 00:15:58 +0300

    redundat unit return type

--- functions/noReturnTypeFunction.kt
+++ functions/noReturnTypeFunction.kt
@@ -4,6 +4,16 @@ unit(void equivalent in java) */
 fun sayHigh(name:String):Unit{
     println("Hello $name")
 }
+
+
+/*
+Unit return type can be omitted.
+ */
+
+ fun sayHighTwice(Greeting:String,name:String){
+    println("$Greeting, $name")
+}
 fun main(args: Array<String>) {
     sayHigh("john")
+    sayHighTwice("Hello myGuy","Kevin")
 }
