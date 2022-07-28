a1751e9af93addbcaa093b6d73a0b3a965caeea5
wanjala stephen <stephenwanjala145@gmail.com>
2022-07-28 22:58:17 +0300

    read only variables

--- dataTypes.kt
+++ dataTypes.kt
@@ -6,4 +6,5 @@ val c: Int // Type required when no initializer is provided
 c = 3
 // deferred assignment
 println("a = $a, b = $b, c = $c")
+//Read-only local variables are defined using the keyword val. They can be assigned a value only once.
 }
