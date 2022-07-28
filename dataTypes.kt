d0f15f24799f210ef3ea7d91a1d281f07459afbf
wanjala stephen <stephenwanjala145@gmail.com>
2022-07-28 23:04:16 +0300

    re_assign  variables

--- dataTypes.kt
+++ dataTypes.kt
@@ -6,5 +6,14 @@ val c: Int // Type required when no initializer is provided
 c = 3
 // deferred assignment
 println("a = $a, b = $b, c = $c")
-//Read-only local variables are defined using the keyword val. They can be assigned a value only once.
+/*Read-only local variables are defined using the keyword val.
+ They can be assigned a value only once.
+ */
+
+ /*
+ Variables that can be reassigned use the var keyword. */
+
+ var x = 23 // `Int` type is inferred by the compiler or explicity specify as var x:Int=23
+x += 1
+println("x = $x")
 }
