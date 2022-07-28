ea54ec37631fe124b7e488a950e2d390f650894b
wanjala stephen <stephenwanjala145@gmail.com>
2022-07-29 00:06:33 +0300

    function with unit return type

---
+++ functions/noReturnTypeFunction.kt
@@ -0,0 +1,9 @@
+
+/*A function that returns no meaningful value.----no return type 
+unit(void equivalent in java) */
+fun sayHigh(name:String):Unit{
+    println("Hello $name")
+}
+fun main(args: Array<String>) {
+    sayHigh("john")
+}
