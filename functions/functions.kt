c2a34ff63d5dd8fb0e2a848d886e24339e177ea8
wanjala stephen <stephenwanjala145@gmail.com>
2022-07-28 23:37:09 +0300

    function

---
+++ functions/functions.kt
@@ -0,0 +1,11 @@
+//*A function with two Int parameters and Int return type. */
+
+fun addTwoNumbers(a:Int,b:Int):Int{
+    return a+b
+}
+fun main(args: Array<String>) {
+    fun main() {
+print("sum of 3 and 5 is ")
+println(addTwoNumbers(3, 5))
+}
+}
