ffb9a81a97bccb58c063f2c1c83a80bb02eb8058
wanjala stephen <stephenwanjala145@gmail.com>
2022-07-29 21:21:44 +0300

    singlr expression functions

---
+++ src/main/kotlin/singleExpressionFunctions/SingleExpressionFunctions.kt
@@ -0,0 +1,17 @@
+package singleExpressionFunctions
+
+/*
+Single-expression functions
+When a function returns a single expression,
+ the curly braces can be omitted and the body is specified after a = symbol:
+ */
+
+fun doubleTheNumber(num:Int):Int=num*2
+
+/*
+Explicitly declaring the return type is optional
+ when this can be inferred by the compiler:
+ as below
+ fun doubleTheNumber(num:Int)=num*2
+ */
+
