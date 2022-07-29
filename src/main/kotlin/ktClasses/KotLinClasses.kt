6cf8ff4b9ed39758c3457683f5acc13cce2198f5
wanjala stephen <stephenwanjala145@gmail.com>
2022-07-29 21:43:05 +0300

    declare classes in kotlin

--- src/main/kotlin/ktClasses/KotLinClasses.kt
+++ src/main/kotlin/ktClasses/KotLinClasses.kt
@@ -2,4 +2,12 @@ package ktClasses
 
 /*
 Classes in Kotlin are declared using the keyword class->class Person{}
+
+The class declaration consists of the class name, the class header (specifying its type parameters, the primary constructor,
+and some other things), and the class body surrounded by curly braces. Both the header and the body are optional; if the
+class has no body, the curly braces can be omitted.
+e.g
+class Person
  */
+
+
