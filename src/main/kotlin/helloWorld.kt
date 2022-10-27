79ef47e85873f648c2742f49fc69f0537415a3fd
wanjala stephen <stephenwanjala145@gmail.com>
2022-10-27 10:09:15 +0300

    hello_world

--- src/main/kotlin/helloWorld.kt
+++ src/main/kotlin/helloWorld.kt
@@ -1,4 +1,7 @@
 fun main(args: Array<String>) {
     println("hello world")
     // or print("hello world") without a new line
+
+
+
 }
--- src/main/kotlin/hello_world.kt
+++ src/main/kotlin/hello_world.kt
@@ -4,6 +4,7 @@ fun main(args: Array<String>) {
 
 println("Hello world!")
 println(42)
+    print(34)
 
 }
     // println prints its arguments and adds a line break, so that the next thing you print appears on the next line.
